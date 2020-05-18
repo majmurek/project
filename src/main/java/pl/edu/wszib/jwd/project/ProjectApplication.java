1e9cb92847ac658cb5fe31d187b5121dd695cade
grzes <majmurek@wszib.edu.pl>
2020-05-18 17:51:10 +0200

    Konfiguracja bazy danych

---
+++ src/main/java/pl/edu/wszib/jwd/project/ProjectApplication.java
@@ -0,0 +1,15 @@
+package pl.edu.wszib.jwd.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("classpath:db.properties")
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
